4cb10e454d4fa97c45e21b25b6680e3d928e9db6
LAPTOP-SSDSJOVC\masah <mr.masa0329@gmail.com>
2026-01-30 17:20:29 +0900

    vite.config.js修正

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        calc: "./index2.html",
+        code: "./code.html"
+      },
+    },
+  },
 });

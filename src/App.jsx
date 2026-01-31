4f82a74f5e0ee812a41c580a997feac82c07de7d
LAPTOP-SSDSJOVC\masah <mr.masa0329@gmail.com>
2026-01-31 15:58:39 +0900

    いろいろ追加

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,28 @@ import {
 const Game = [
     {
         name: "ブルーアーカイブ -Blue Archive-",
-        genre: "RPG",
+        genre: ["RPG"],
         model: "iOS,Android",
+        seller: "Yostar, Inc.",
+        link: "https://bluearchive.jp/",
         download: 13000000,
         year: "2021",
         month: "2",
         season: "冬",
         image: "Game-Genre/RPG/ブルーアーカイブ.jpg"
     },
+    {
+        name: "アズールレーン",
+        genre: ["RPG,シューティング"],
+        model: "iOS,Android",
+        seller: "Yostar, Inc.",
+        link: "https://www.azurlane.jp/",
+        download: 40000000,
+        year: "2017",
+        month: "9",
+        season: "夏",
+        image: "Game-Genre/RPG/アズールレーン.jpg"
+    }
 ];
 export default function App() {
   return (

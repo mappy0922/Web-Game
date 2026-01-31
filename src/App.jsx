a672a9d682cc2fb712c9baed33cd1c73ee0e029a
LAPTOP-SSDSJOVC\masah <mr.masa0329@gmail.com>
2026-01-31 16:53:11 +0900

    ゲーム追加

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,31 @@ const Game = [
         month: 9,
         season: "夏",
         image: "Game-Genre/RPG/アズールレーン.jpg"
-    }
+    },
+    {
+        name: "Fate/Grand Order",
+        genre: ["RPG"],
+        model: "iOS,Android",
+        seller: "Aniplex Inc.",
+        link: "https://www.fate-go.jp/",
+        download: 80000000,
+        year: 2015,
+        month: 7,
+        season: "夏",
+        image: "Game-Genre/RPG/Fate Grand Order.jpg"
+    },
+    {
+        name: "ファイナルファンタジーXIV",
+        genre: ["RPG"],
+        model: "PS4,PS5,PC,Xbox",
+        seller: "SQUARE ENIX, Inc.",
+        link: "https://www.finalfantasyxiv.com/freetrial/?utm_source=accesstrade&utm_medium=affiliate&utm_campaign=jp-freetrial&utm_content=affiliate-1&atnct=seff14_0100mmzp009j64-ff151fed48cbe91e97418554175f1e60",
+        download: 30000000,
+        year: 2013,
+        month: 8,
+        season: "夏",
+        image: "Game-Genre/RPG/ファイナルファンタジーXIV.jpg"
+    },
 ];
 export default function App() {
   return (

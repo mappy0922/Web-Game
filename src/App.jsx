94e86f76dd0efd4676d09c3e5ee017cb254c1c25
LAPTOP-SSDSJOVC\masah <mr.masa0329@gmail.com>
2026-01-31 14:18:06 +0900

    App.jsx修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import { useState } from "react";
+import {
+  Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis,
+  BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend
+} from "recharts";
 
+const Game = [
+    {
+        name: "ブルーアーカイブ -Blue Archive-",
+        genre: "RPG",
+        model: "iOS,Android",
+        download: 13000000,
+        year: "2021",
+        month: "2",
+        season: "冬",
+        image: "Game-Genre/RPG/ブルーアーカイブ.jpg"
+    },
+];
 export default function App() {
   return (
     <div>
